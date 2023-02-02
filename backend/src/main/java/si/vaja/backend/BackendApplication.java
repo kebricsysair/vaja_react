582ce0af9af9e45a7f6fbf1d59cf228bc4006db5
kebricsysair <klara.kebric@systemair.si>
2023-02-02 13:05:35 +0100

    backend java 8

--- backend/src/main/java/si/vaja/backend/BackendApplication.java
+++ backend/src/main/java/si/vaja/backend/BackendApplication.java
@@ -1,13 +1,23 @@
 package si.vaja.backend;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class BackendApplication {
+public class BackendApplication implements CommandLineRunner {
 
+	@Autowired
+	private JdbcTemplate template;
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
+	@java.lang.Override
+	public void run(java.lang.String... args) throws Exception {
+
+		template.update("INSERT INTO Uporabnik(Ime, Priimek, Starost) VALUES ('Klara', 'Kebric', 20)");
+	}
 }
